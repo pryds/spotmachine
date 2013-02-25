b8e2e4244427123897e97e65d1dff58dd67193d2
lauritsen <lauritsen@c56fde60-a67d-411e-aac6-0c9094d3ee9b>
2013-02-25 20:47:39 +0000

    Debian package: installed package size; get name and version number in control file from packagedeb.sh

--- src/packagedeb.sh
+++ src/packagedeb.sh
@@ -11,7 +11,10 @@ mkdir -p $TEMP_DIR/debian/bin
 mkdir -p $TEMP_DIR/debian/usr/share/applications
 mkdir -p $TEMP_DIR/debian/usr/share/doc/$PACKAGE_NAME
 
-cp debian/control $TEMP_DIR/debian/DEBIAN/
+echo "Package: $PACKAGE_NAME" > $TEMP_DIR/debian/DEBIAN/control
+echo "Version: $PACKAGE_VERSION" >> $TEMP_DIR/debian/DEBIAN/control
+cat debian/control >> $TEMP_DIR/debian/DEBIAN/control
+
 cp debian/spotmachine.desktop $TEMP_DIR/debian/usr/share/applications/
 cp debian/copyright $TEMP_DIR/debian/usr/share/doc/$PACKAGE_NAME/
 cp -r ../bin/ $TEMP_DIR/debian/lib/$PACKAGE_NAME
@@ -21,11 +24,17 @@ chmod 644 $TEMP_DIR/debian/usr/share/doc/$PACKAGE_NAME/spotmachine.svg
 rm -r $TEMP_DIR/debian/lib/$PACKAGE_NAME/debian
 rm $TEMP_DIR/debian/lib/$PACKAGE_NAME/COPYING
 rm $TEMP_DIR/debian/lib/$PACKAGE_NAME/packagedeb.sh
+rm $TEMP_DIR/debian/lib/$PACKAGE_NAME/$PACKAGE_NAME-$PACKAGE_VERSION.deb
+rm $TEMP_DIR/debian/lib/$PACKAGE_NAME/winInstaller.nsi
 
 echo '#!/bin/sh' > $TEMP_DIR/debian/bin/spotmachine
 echo "export CLASSPATH=$CLASSPATH:/lib/$PACKAGE_NAME" >> $TEMP_DIR/debian/bin/spotmachine
 echo "java main/SpotMachine $1" >> $TEMP_DIR/debian/bin/spotmachine
 chmod 755 $TEMP_DIR/debian/bin/spotmachine
+
+PACKAGE_SIZE=`du -bs $TEMP_DIR/debian | cut -f 1`
+PACKAGE_SIZE=$((PACKAGE_SIZE/1024))
+echo "Installed-Size: $PACKAGE_SIZE" >> $TEMP_DIR/debian/DEBIAN/control
 
 chown -R root $TEMP_DIR/debian/
 chgrp -R root $TEMP_DIR/debian/
